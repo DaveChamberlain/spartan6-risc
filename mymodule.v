64eb50948412bd3d837633f818d92dd9acc42c75
Dave Chamberlain <dchamberlain@edx.org>
2017-03-30 10:28:57 -0400

    Merged the new code into the framework. Want to test, but boy does it need to be cleaned up

--- mymodule.v
+++ mymodule.v
@@ -51,70 +51,7 @@ module mymodule(
 	 input DPSwitch[0:7]
     );
 
-/*
-	//reg	clk=0;
 
-	reg [7:0] r[0:3];	//4 registers A; m[0]=B; m[0]=C; m[0]=D
-	reg [7:0] pc;		//program counter
-	reg [7:0] m[0:255];	//memory
-	
-	reg [20:0] ctr;
-	reg [2:0] stage;
-	
-	reg [7:0] ir;
-	reg [3:0] opc4;
-	reg [1:0] opc2;
-	reg [1:0] rs;
-	reg [1:0] rd;
-	reg [1:0] rt;
-	reg [3:0] imm;
-	
-	reg [1:0] lock;
-
-	assign LED[0] = m[245][0:0];
-	assign LED[1] = m[245][1:1];
-	assign LED[2] = m[245][2:2];
-	assign LED[3] = m[245][3:3];
-	assign LED[4] = m[245][4:4];
-	assign LED[5] = m[245][5:5];
-	assign LED[6] = m[245][6:6];
-	assign LED[7] = m[245][7:7];
-	
-	wire [7:0] inst;	//quick access to the current instruction
-	wire [7:0] pc_in;
-	
-	assign pc_in = pc + 1;
-	
-	//the current instruction is set to be the memory contents at pc
-	assign inst=m[pc];
-
-	initial begin
-		//$display("starting...");
-
-		stage=0;
-		pc=0;		//set all the registers to 0
-		r[0]=0;
-		r[1]=0;
-		r[2]=0;
-		r[3]=0;
-		
-		lock =0;
-		
-		ctr = 0;
-
-		// Finuacci code
-   
-		m[00]='hbf; m[01]='hf0; m[02]='h90; m[03]='hd0; m[04]='h37; m[05]='hff; m[06]='h37; 
-		m[07]='hf1; m[08]='hd1; m[09]='h37; m[10]='hf0; m[11]='h23; m[12]='hf1; m[13]='h27; 
-		m[14]='h2b; m[15]='h08; m[16]='hff; m[17]='h3b; m[18]='hf0; m[19]='h37; m[20]='h23; 
-		m[21]='hff; m[22]='h27; m[23]='hf1; m[24]='h37; m[25]='ha0; m[26]='he1; m[27]='hfe; 
-		m[28]='h27; m[29]='h16; m[30]='h37; m[31]='h16; m[32]='ha2; m[33]='he6; m[34]='h49; 
-		m[35]='hea; m[36]='ha0; m[37]='h5a; m[38]='h70; 
-
-		m[254]=6;	//memory[FE] is 6
-
-	end
-*/
 
 reg[7:0] PC;
 reg[7:0] instruction;
@@ -126,6 +63,7 @@ reg[7:0] D;
 reg[7:0] results;
 reg[7:0] Rrd;
 reg[7:0] Rrs;
+reg[7:0] Rrt;
 reg clk = 0;
 
 reg[7:0] cycle = 0;
@@ -133,11 +71,40 @@ reg[7:0] cycle = 0;
 wire[7:0] instruction_in;
 assign instruction_in = memory[PC];
 
+wire[7:0] dataMemory_in;
+assign dataMemory_in = memory[Rrs_in];
+
 wire[1:0] rd;
 assign rd=instruction[3:2];
 
 wire[1:0] rs;
 assign rs=instruction[1:0];
+
+wire[1:0] rt;
+assign rt=instruction[5:4];
+
+wire[3:0] immediate = instruction[3:0];
+
+wire[0:0] doJump = ((Rrs <= 0) || ((Rrs & 'h80) != 0)) ? 1 : 0;
+
+wire[1:0] opCode2;
+assign opCode2=instruction[7:6];
+
+wire[3:0] opCode4;
+assign opCode4=instruction[7:4];
+
+wire[1:0] wbRdOrRt;
+assign wbRdOrRt = (opCode2 == 2 || opCode2 == 3) ? 1 :
+                  (opCode4 == 2 || opCode4 == 3) ? 0 :
+                  (opCode4 == 4 || opCode4 == 5) ? 2 : 0;
+
+wire[1:0] resultsMux;
+assign resultsMux = (opCode4 == 0 || opCode4 == 1) ? 0 :
+                     opCode4 == 2 ? 3 :
+                     opCode2 == 2 ? 1 :
+                     opCode2 == 3 ? 2 : 0;
+wire[7:0] Rrt_in;
+assign Rrt_in = rt==0 ? A : rt==1 ? B : rt==2 ? C : D;
 
 wire[7:0] Rrd_in;
 assign Rrd_in = rd==0 ? A : rd==1 ? B : rd==2 ? C : D;
@@ -154,34 +121,45 @@ assign register_in = results;
 reg [20:0] ctr;
 reg [0:0] lock;
 
-wire[7:0] result_in = Rrd + Rrs;
+wire[7:0] ALU = opCode4==0 ? Rrd + Rrs : opCode4==1 ? Rrd - Rrs : 0;
+wire[7:0] joinerHigh = {immediate[3:0],Rrt_in[3:0]};
+wire[7:0] joinerLow = {Rrt_in[7:4],immediate[3:0]};
 
-	assign LED[0] = memory[245][0:0];
-	assign LED[1] = memory[245][1:1];
-	assign LED[2] = memory[245][2:2];
-	assign LED[3] = memory[245][3:3];
-	assign LED[4] = memory[245][4:4];
-	assign LED[5] = memory[245][5:5];
-	assign LED[6] = memory[245][6:6];
-	assign LED[7] = memory[245][7:7];
+wire[7:0] result_in;
+assign result_in = resultsMux == 0 ? ALU : 
+                   resultsMux == 1 ? joinerHigh : 
+                   resultsMux == 2 ? joinerLow :
+                   resultsMux == 3 ? dataMemory_in :
+                   results;
+
+assign LED[0] = memory[255][0:0];
+assign LED[1] = memory[255][1:1];
+assign LED[2] = memory[255][2:2];
+assign LED[3] = memory[255][3:3];
+assign LED[4] = memory[255][4:4];
+assign LED[5] = memory[255][5:5];
+assign LED[6] = memory[255][6:6];
+assign LED[7] = memory[255][7:7];
 	
 
 initial begin
    PC=0;
-	lock=0;
+   lock=0;
 
    A = 3;
    B = 4;
    C = 5;
    D = 6;
 
-   memory[0] = 01;
-   memory[1] = 00;
-   memory[2] = 04;
-   memory[3] = 00;
-   memory[4] = 'h70;
-	
-	memory[245] = 255;
+        A=0; B=0; C=0; D=0;
+        memory[00]='hbf; memory[01]='hf0; memory[02]='h90; memory[03]='hd0; memory[04]='h37; memory[05]='hff; memory[06]='h37;
+        memory[07]='hf1; memory[08]='hd1; memory[09]='h37; memory[10]='hf0; memory[11]='h23; memory[12]='hf1; memory[13]='h27;
+        memory[14]='h2b; memory[15]='h08; memory[16]='hff; memory[17]='h3b; memory[18]='hf0; memory[19]='h37; memory[20]='h23;
+        memory[21]='hff; memory[22]='h27; memory[23]='hf1; memory[24]='h37; memory[25]='ha0; memory[26]='he1; memory[27]='hfe;
+        memory[28]='h27; memory[29]='h16; memory[30]='h37; memory[31]='h16; memory[32]='ha2; memory[33]='he6; memory[34]='h49;
+        memory[35]='hea; memory[36]='ha0; memory[37]='h5a; memory[38]='h70;
+
+        memory[254]=6;
 end
 
 //always #1 clk=~clk;
@@ -190,7 +168,7 @@ always @(posedge CLK_12MHz) begin
 	   ctr = ctr + 1;
 		
 		if (!Switch[0] & !lock) begin
-		    lock <= 1;
+		    lock <= 0;
 		    cycle <= `FETCH;
 			 end
 		else if(Switch[0] & lock) begin
@@ -222,10 +200,13 @@ always @(posedge CLK_12MHz) begin
 			 end
 
 		if (!Switch[1]) begin
-			 A <= 3;
-			 B <= 4;
-			 C <= 5;
-			 D <= 6;
+			 A <= 0;
+			 B <= 0;
+			 C <= 0;
+			 D <= 0;
+			 
+			 memory[255] = 0;
+			 memory[254] = 6;
 			 
 		    cycle <= `START;
 			 end
@@ -234,42 +215,81 @@ always @(posedge CLK_12MHz) begin
 		case(cycle)
       `START: 
          begin
-			   PC <= 0;
+			PC <= 0;
 				
             cycle <= `FETCH;
          end
       `FETCH: 
          begin
-            PC <= PC_in;
-            instruction <= instruction_in;
-            cycle <= `DECODE;
+            if ((opCode4 == 4 && doJump) || opCode4 == 5) begin
+               end
+            if (opCode4 == 4 && doJump) begin
+               PC <= Rrd_in;
+               instruction <= memory[Rrd_in];
+               cycle <= `DECODE;
+               end
+            else begin
+               PC <= PC_in;
+               instruction <= instruction_in;
+               cycle <= `DECODE;
+               end
          end
       `DECODE: 
          begin
             Rrd <= Rrd_in;
             Rrs <= Rrs_in;
+            Rrt <= Rrt_in;
             cycle <= `EXECUTE;
          end
       `EXECUTE: 
          begin
-            results <= result_in;
-            cycle <= `WRITEBACK;
+            if (opCode4 == 3) begin
+               memory[Rrs] <= Rrd;
+               cycle <= `FETCH;
+               end
+            else if (opCode4 == 4) begin
+               cycle <= `FETCH;
+               end
+            else if (opCode4 == 5) begin
+               results <= Rrd_in;
+               cycle <= `WRITEBACK;
+               end
+            else begin
+               cycle <= `WRITEBACK;
+               results <= result_in;
+               end
          end
       `WRITEBACK: 
          begin
-            case(rd)
-                  0: A <= register_in;
-                  1: B <= register_in;
-                  2: C <= register_in;
-                  3: D <= register_in;
+            case(wbRdOrRt)
+               0: case(rd)
+                     0: A <= register_in;
+                     1: B <= register_in;
+                     2: C <= register_in;
+                     3: D <= register_in;
+                  endcase
+               1: case(rt)
+                     0: A <= register_in;
+                     1: B <= register_in;
+                     2: C <= register_in;
+                     3: D <= register_in;
+                  endcase
+               2: begin
+                  case(rs)
+                     0: A <= PC_in;
+                     1: B <= PC_in;
+                     2: C <= PC_in;
+                     3: D <= PC_in;
+                  endcase
+                  PC <= Rrd_in;
+                  end
             endcase
-            cycle <= `PAUSE;
-				lock <= 1;
+            cycle <= `FETCH;
          end
 		`PAUSE:
 		   begin
-			   memory[245] <= PC;
-			end
+			   LED[7] = !LED[7]
+		   end
    endcase
 
    //$display("A=%h B=%h C=%h D=%h cycle=%h PC=%h instruction=%h",
@@ -279,131 +299,4 @@ always @(posedge CLK_12MHz) begin
 	end
 end
 
-
-
-
-/*
-	//instructions are executed on the positive clock edge
-	always @(posedge CLK_12MHz) begin
-	
-	   ctr = ctr + 1;
-		
-		if (!Switch[0] & !lock) begin
-		    lock <= 1;
-		    stage <= `FETCH;
-			 end
-		else if(Switch[0] & lock) begin
-		    lock <= 0;
-			 end
-		else if (!Switch[2] & !lock) begin
-		    if (DPSwitch[0])
-			     m[245]<=rd;
-		    else
-			     m[245]<=r[0];
-			 end
-		else if (!Switch[3] & !lock) begin
-		    if (DPSwitch[0])
-			     m[245]<=rs;
-		    else
-			     m[245]<=r[1];
-			 end
-		else if (!Switch[4] & !lock) begin
-		    if (DPSwitch[0])
-			     m[245]<=rt;
-		    else
-			     m[245]<=r[2];
-			 end
-		else if (!Switch[5] & !lock) begin
-		    if (DPSwitch[0]) 
-			     m[245]<=imm;
-		    else
-			     m[245]<=r[3];
-			 end
-
-		if (!Switch[1]) begin
-		    stage <= `START;
-			 end
-		
-		if (ctr == 0 & !lock) begin
-		    case (stage)
-		        `START: begin
-				      m[245]<='hF0;
-						m[254]<=6;
-						m[255]<=0;
-				      stage <= `PAUSE;
-						pc <= 0;
-				      end
-				  `FETCH: begin
-				      pc <= pc_in;
-						ir <= inst;
-						stage <= `DECODE;
-				      end
-				  `DECODE: begin
-						opc4 <= ir[`OPCODE_4];
-						opc2 <= ir[`OPCODE_2];
-						rs <= ir[`INST_RS];
-						rd <= ir[`INST_RD];
-						rt <= ir[`INST_RT];
-						imm <= ir[`INST_IMM];
-				      stage <= `EXECUTE;
-						m[245]<=ir;
-				      end
-				  `EXECUTE: begin 
-					   case(opc4)
-						    `ADD: begin
-							     r[rd]<=r[rd]+r[rs];
-								  stage <= `WRITEBACK;
-							     end
-						    `SUB: begin
-							     r[rd]<=r[rd]-r[rs];
-								  stage <= `WRITEBACK;
-							     end
-						    `LOAD: begin
-				              r[rd]<=m[r[rs]];
-								  stage <= `WRITEBACK;
-							     end
-						    `STORE: begin
-				              m[r[rs]]<=r[rd];
-								  stage <= `WRITEBACK;
-							     end
-						    `JLEZ: begin
-                          if (r[rs] <= 0)
-				                  pc<=r[rd];
-                          else
-				                  pc<=pc+1;
-								  stage <= `FETCH;
-								  end
-						    `JALR: begin
-                          r[rs]<=pc+1;
-				              pc<=r[rd];
-								  stage <= `FETCH;
-								  end
-						    `HALT: begin
-                          stage <= `DONE;
-								  end
-						    endcase 
-			         case (opc2)
-			             `LUI:	begin
-				              r[rt]<={imm,r[rt][3:0]};
-					           stage <= `PAUSE;
-				              end
-			             `LLI:	begin
-				               r[rt]<={r[rt][7:4],imm};
-					            stage <= `PAUSE;
-				               end			 
-				          endcase
-				      end 
-				  `WRITEBACK: begin
-				      stage <= `FETCH;
-						end
-				  `DONE: begin
-				      m[245] <= m[255];
-				      end
-						
-			 endcase
-		end
-
-
-	end
-*/
 endmodule
